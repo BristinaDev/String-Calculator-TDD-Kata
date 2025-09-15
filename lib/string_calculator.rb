6c29bf9c5a9c2f6dc8bf1cf69eb45a1b76583ec1
my game <srujal@srujals-MacBook-Air.local>
2025-09-15 15:49:43 +0530

    test: Handle single number input Support adding two comma separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,10 @@
 class StringCalculator 
   def self.add(numbers)
-    return 0
+    return 0 if numbers.nil? || numbers == ""
+
+		answer = numbers.split(",").map(&:to_i)
+		return answer[0] if answer.length == 1
+
+		return answer.sum
 	end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
 		it "return 0 for empty string" do
 		  expect(StringCalculator.add("")).to eq(0)
 		end
+
+		it "return the number for single input" do
+			expect(StringCalculator.add("1")).to eq(1)
+		end
+	
+		it "adds two comma separated numbers" do
+			expect(StringCalculator.add("1,5")).to eq(6)
+		end
 	end
 end
